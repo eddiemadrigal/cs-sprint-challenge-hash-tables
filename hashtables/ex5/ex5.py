76636ef9216f6ac5301db0cf43ec4d233c5023f7
Eddie Madrigal <edmadrigal@yahoo.com>
2020-08-07 10:34:06 -0700

    ex5.py file updated

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,15 +1,22 @@
 # Your code here
 
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    result = []                                 # define result list
+    cache = {}                                  # define cache dictionary
+    for path in files:                          # check all paths
+        if path in cache:                       # if the path exists in cache
+            result.append(path)                 # append the path to the cache dictionary
+        pathSplit = path.split('/')             # split string at '/' to create list
+        lastItem = pathSplit[len(pathSplit) - 1]# get last item in the list
+        if lastItem in queries:                 # if the last item in the array matches what is in the queries
+            result.append(path)                 # append the path to the resuls list (line 8)
 
-    return result
-
+    return result                               # return the result
+ 
 
 if __name__ == "__main__":
     files = [
