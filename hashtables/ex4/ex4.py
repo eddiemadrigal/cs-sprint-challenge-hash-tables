c69d8322bbfc590bf8e9fa0c6adf23eca815b745
Eddie Madrigal <edmadrigal@yahoo.com>
2020-08-07 10:49:47 -0700

    file solution added

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,16 @@
+from collections import Counter
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
     # Your code here
+    mydict = Counter()      # my dictionary defined by Counter()
+    for i in a:             # loop through each number element in the passed in list
+        mydict[i] += 1      # for each element found, increment index
 
-    return result
+    result = [i for i in mydict.keys() if i > 0 and mydict[-i] > 0] # result = mydict keys if i > 0 and there is a negative equivalent
+
+    return result   # return values found that are positive and have corresponding negative values
 
 
 if __name__ == "__main__":
